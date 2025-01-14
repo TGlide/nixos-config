51d639cba30b91dd585e399159871609fdfba31d
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-01-14 18:18:11 +0000

    add fzf

--- home-manager/shared.nix
+++ home-manager/shared.nix
@@ -66,15 +66,13 @@
       neofetch
     '';
     plugins = [
-      # Manually packaging and enable a plugin
+      {
+        name = "fzf";
+        src = pkgs.fishPlugins.fzf;
+      }
       {
         name = "z";
-        src = pkgs.fetchFromGitHub {
-          owner = "jethrokuan";
-          repo = "z";
-          rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
-          sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
-        };
+        src = pkgs.fishPlugins.z;
       }
     ];
     shellAliases = {
