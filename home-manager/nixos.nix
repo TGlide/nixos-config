137e8f3936d53a76d09805d32f1c8156109e7ffa
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-01-31 14:09:18 +0000

    change media output command

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -111,5 +111,8 @@
       nvim /etc/nixos
       sudo chown -R root /etc/nixos
     '';
+    change_media_output = ''
+      wpctl status | sed -n "/Sinks:/,/Sources:/p" | string replace -a "├" "" | string replace -a "─" "" | string replace -a "│" "" | string replace -a "└" "" | string match -r ".*\[vol:.*" | string replace -r "\s*\[vol:.*$" "" | string replace -r "^\s*(\d+)\." "$1\t" | string replace -r ".*?\\*.*?(\\d+).*?(\\w.*$)" "<b>$1 $2 *</b>" | string replace -r "\s+" " " | wofi --show=dmenu --hide-scroll --allow-markup | read -l choice; and wpctl set-default (string match -r "^\d+" "$choice")
+    '';
   };
 }
