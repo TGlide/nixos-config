77856b9f15e4873a68f3ede397071a3e0f8334d5
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-01-31 14:13:01 +0000

    fix output

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -112,7 +112,7 @@
       sudo chown -R root /etc/nixos
     '';
     change_media_output = ''
-      wpctl status | sed -n "/Sinks:/,/Sources:/p" | string replace -a "├" "" | string replace -a "─" "" | string replace -a "│" "" | string replace -a "└" "" | string match -r ".*\[vol:.*" | string replace -r "\s*\[vol:.*$" "" | string replace -r "^\s*(\d+)\." "$1\t" | string replace -r ".*?\\*.*?(\\d+).*?(\\w.*$)" "<b>$1 $2 *</b>" | string replace -r "\s+" " " | wofi --show=dmenu --hide-scroll --allow-markup | read -l choice; and wpctl set-default (string match -r "^\d+" "$choice")
+      wpctl status | sed -n \'/Sinks:/,/Sources:/p\' | string replace -a \'├\' \'\' | string replace -a \'─\' \'\' | string replace -a \'│\' \'\' | string replace -a \'└\' \'\' | string match -r \'.*\[vol:.*\' | string replace -r \'\s*\[vol:.*$\' \'\' | string replace -r \'^\s*\*?\s*(\d+)\.\' \'$1\t\' | string replace -r \'^(\d+\t.*) \(Default\)$|^(\d+\t.*)\s*$\' \'$1$2\' | awk \'{if($0~/\*/) print "<b>"$0"</b>"; else print $0}\' | wofi --show=dmenu --hide-scroll --allow-markup --location=top_right --width=600 --height=200 --xoffset=-60 | read -l choice; and wpctl set-default (string match -r \'^\d+\' "$choice")
     '';
   };
 }
