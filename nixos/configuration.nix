29d8d0d050adaf2165ece80a91739ba2cbe4863a
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-02-10 11:33:48 +0000

    add gamescope

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -198,6 +198,9 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession = {
+      enable = true;
+    };
   };
 
   # Allow unfree packages
