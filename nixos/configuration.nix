563db39252c01e6e7a3700b7a75cf742cb74e473
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-02-11 13:15:06 +0000

    add gnome back in for utils

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -102,7 +102,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
